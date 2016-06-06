f1f9be19d79353870bb9c799ecb08cf35953f736
Oakula <cal.warrilow@gmail.com>
2016-06-06 09:13:49 +0100

    added MainFrame class and .gitignore file

---
+++ MainFrame.java
@@ -0,0 +1,37 @@
+/* 
+	Classname: QuadrantUsr
+	Author: Callum Warrilow
+	Desc: User class to allow interaction with the QUAD app GUI
+*/
+
+// ----- IMPORT STATEMENTS -----
+import java.awt.*;
+import javax.swing.*;
+
+public class MainFrame extends JFrame {
+	
+	// ----- CONSTRUCTOR -----
+	public MainFrame(){
+		super("QUAD");		
+		setMinimumSize(new Dimension(700, 450));
+		setResizable(true);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		setLayout(new GridBagLayout());
+
+		buildGUI();
+		setVisible(true);
+	} // end of CONSTRUCTOR
+
+	private void buildGUI(){
+		Quadrant quad = new Quadrant();	
+
+		GridBagConstraints quadC = new GridBagConstraints();
+		quadC.gridx = 10;
+        quadC.gridy = 8;
+        quadC.weightx = 0;
+        quadC.weighty = 0;
+
+		add(quad, quadC);
+	} // end of main() method
+}
